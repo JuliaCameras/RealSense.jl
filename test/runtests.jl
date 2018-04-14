4e98af535135e5722c8622879f848f3976b107c0
Gnimuc <qiyupei@gmail.com>
2018-04-14 14:18:49 +0800

    Add a naive test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,7 @@
 using RealSense
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+err = Ref{Ptr{rs2_error}}(0)
+ctx = rs2_create_context(RS2_API_VERSION, err)
+@test err[] == C_NULL
+rs2_delete_context(ctx)
