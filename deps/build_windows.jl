ab2532bcb592b9043d803d45f17c80094d93ab5a
Gnimuc <qiyupei@gmail.com>
2018-09-16 23:27:28 +0800

    Fix AppVeyor

--- deps/build_windows.jl
+++ deps/build_windows.jl
@@ -8,9 +8,9 @@ locations = [WINDOWS_DEFAULT_DIR]
 # try to find the lib
 lib = Libdl.find_library(["realsense2"], locations)
 if lib == ""
-    error("Couldn't find realsense2.dll in the default directory $WINDOWS_DEFAULT_DIR. Please install Intel RealSense SDK 2.0 and then run Pkg.build(\"RealSense\").")
+    error("Couldn't find realsense2.dll in the default directory $WINDOWS_DEFAULT_DIR. Please install Intel RealSense SDK 2.0 and then run Pkg.build(\"LibRealSense\").")
 else
-    const librealsense = Libdl.dlpath(lib) |> x->replace(x, "\\", "\\\\")
+    const librealsense = Libdl.dlpath(lib) |> x->replace(x, "\\"=>"\\\\")
 end
 
 
@@ -24,25 +24,25 @@ open(joinpath(@__DIR__, "deps.jl"), "w") do f
     function check_deps()
         global librealsense2
         if !isfile(librealsense2)
-            error("\$(librealsense2) does not exist, Please re-run Pkg.build(\\"RealSense\\"), and restart Julia.")
+            error("\$(librealsense2) does not exist, Please re-run Pkg.build(\\"LibRealSense\\"), and restart Julia.")
         end
 
         hdl = Libdl.dlopen_e(librealsense2)
         if hdl == C_NULL
-            error("\$(librealsense2) cannot be opened, Please re-run Pkg.build(\\"RealSense\\"), and restart Julia.")
+            error("\$(librealsense2) cannot be opened, Please re-run Pkg.build(\\"LibRealSense\\"), and restart Julia.")
         else
             get_api_version = Libdl.dlsym_e(hdl, :rs2_get_api_version)
             @assert get_api_version != C_NULL "Could not find `rs2_get_api_version` within \$(librealsense2)."
-            err = Ref{Ptr{Void}}(0)
-            version = ccall(get_api_version, Cint, (Ptr{Ptr{Void}},), err)
+            err = Ref{Ptr{Cvoid}}(0)
+            version = ccall(get_api_version, Cint, (Ptr{Ptr{Cvoid}},), err)
             @assert err[] == C_NULL "Something happened when calling `rs2_get_api_version`."
             major = version ÷ 10000
             minor = (version - 10000*major) ÷ 100
             patch = (version - 10000*major - 100*minor)
             if major != LIBREALSENSE_VERSION.major && minor != LIBREALSENSE_VERSION.minor
                 error(\"\"\"API version mismatch:
-                      The librealsense's API version is v\$(major).\$(minor).\$(patch), but the one used in RealSense.jl is \$(LIBREALSENSE_VERSION).
-                      Please either upgrade Intel RealSense SDK or RealSense.jl.\"\"\")
+                      The system librealsense's API version is v\$(major).\$(minor).\$(patch), but the one used in LibRealSense.jl is \$(LIBREALSENSE_VERSION).
+                      Please either upgrade Intel RealSense SDK or LibRealSense.jl.\"\"\")
             end
         end
     end
